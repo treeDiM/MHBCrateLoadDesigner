7dc4e4f17fead39aff5211870ee11e725b334f8d
François Gasnier <fgasnier71@gmail.com>
2021-02-11 11:56:06 +0100

    Corrected Issue #1

--- Sources/MHB.CrateLoadDesigner/MHB.CreateLoadDesigner.Engine/InputFileReader.cs
+++ Sources/MHB.CrateLoadDesigner/MHB.CreateLoadDesigner.Engine/InputFileReader.cs
@@ -32,27 +32,36 @@ namespace MHB.CrateLoadDesigner.Engine
 
                                 if (iRow > 2)
                                 {
-                                    string sDataTypeActual = string.Empty;
+                                    Type dataTypeActual = null;
                                     string sDataTypeExpected = string.Empty;
                                     try
                                     {
-                                        sDataTypeExpected = "string"; sDataTypeExpected = reader.GetFieldType(0).ToString();
-                                        string projNameRow = reader.GetString(0);  
-                                        sDataTypeExpected = "string"; sDataTypeExpected = reader.GetFieldType(1).ToString();
+                                        sDataTypeExpected = "string"; dataTypeActual = reader.GetFieldType(0);
+                                        string projNameRow = string.Empty;
+                                        if (null != dataTypeActual)
+                                        {
+                                            switch (dataTypeActual.ToString())
+                                            {
+                                                case "system.string": projNameRow = reader.GetString(0); break;
+                                                default: projNameRow = reader.GetValue(0).ToString(); break;
+                                            }
+                                        }
+                                        sDataTypeExpected = "string"; dataTypeActual = reader.GetFieldType(1);
                                         string brand = reader.GetString(1);
-                                        sDataTypeExpected = "double"; sDataTypeExpected = reader.GetFieldType(2).ToString();
+                                        sDataTypeExpected = "double"; dataTypeActual = reader.GetFieldType(2);
                                         int number = (int)reader.GetDouble(2);
-                                        sDataTypeExpected = "double"; sDataTypeExpected = reader.GetFieldType(3).ToString();
+                                        sDataTypeExpected = "double"; dataTypeActual = reader.GetFieldType(3);
                                         double width = reader.GetDouble(3);
-                                        sDataTypeExpected = "double"; sDataTypeExpected = reader.GetFieldType(4).ToString();
+                                        sDataTypeExpected = "double"; dataTypeActual = reader.GetFieldType(4);
                                         double height = reader.GetDouble(4);
-                                        sDataTypeExpected = "string"; sDataTypeExpected = reader.GetFieldType(5).ToString();
+                                        sDataTypeExpected = "string"; dataTypeActual = reader.GetFieldType(5);
                                         string description = reader.GetString(5);
                                         listFrames.Add(new DefFrame() { Brand = brand, Description = description, Width = width, Height = height, Number = number });
                                     }
                                     catch (Exception /*ex*/)
                                     {
-                                        throw new Exception($"Sheet: {SheetNameFrame} - Row: {iRow} => Invalid field type -> Expected : Actual : {sDataTypeActual}");
+                                        string sDataTypeActual = (null != dataTypeActual ? dataTypeActual.ToString() : "null");
+                                        throw new Exception($"Sheet: {SheetNameFrame} - Row: {iRow} => Invalid field type -> Expected: {sDataTypeExpected}  Actual: {sDataTypeActual}");
                                     }
                                 }
                                 ++iRow;
@@ -65,26 +74,36 @@ namespace MHB.CrateLoadDesigner.Engine
                             {
                                 if (iRow > 0)
                                 {
-                                    string sDataTypeActual = string.Empty;
+                                    Type dataTypeActual = null;
                                     string sDataTypeExpected = string.Empty;
 
                                     try
                                     {
-                                        sDataTypeExpected = "string"; sDataTypeExpected = reader.GetFieldType(0).ToString();
-                                        string projNameRow = reader.GetString(0);
-                                        sDataTypeExpected = "string"; sDataTypeExpected = reader.GetFieldType(1).ToString();
+
+                                        sDataTypeExpected = "string"; dataTypeActual = reader.GetFieldType(0);
+                                        string projNameRow = string.Empty;
+                                        if (null != dataTypeActual)
+                                        {
+                                            switch (dataTypeActual.ToString())
+                                            {
+                                                case "system.string": projNameRow = reader.GetString(0); break;
+                                                default: projNameRow = reader.GetValue(0).ToString(); break;
+                                            }
+                                        }
+                                        sDataTypeExpected = "string"; dataTypeActual = reader.GetFieldType(1);
                                         string brand = reader.GetString(1);
-                                        sDataTypeExpected = "double"; sDataTypeExpected = reader.GetFieldType(2).ToString();
+                                        sDataTypeExpected = "double"; dataTypeActual = reader.GetFieldType(2);
                                         int number = (int)reader.GetDouble(2);
-                                        sDataTypeExpected = "string"; sDataTypeExpected = reader.GetFieldType(3).ToString();
+                                        sDataTypeExpected = "string"; dataTypeActual = reader.GetFieldType(3);
                                         double width = reader.GetDouble(3);
-                                        sDataTypeExpected = "string"; sDataTypeExpected = reader.GetFieldType(4).ToString();
+                                        sDataTypeExpected = "string"; dataTypeActual = reader.GetFieldType(4);
                                         double height = reader.GetDouble(4);
                                         listGlass.Add(new DefGlass() { Brand = brand, Width = width, Height = height, Number = number });
                                     }
                                     catch (Exception /*ex*/)
                                     {
-                                        throw new Exception($"Sheet: {SheetNameFrame} - Row: {iRow} => Invalid field type -> Expected : Actual : {sDataTypeActual}");
+                                        string sDataTypeActual = (null != dataTypeActual ? dataTypeActual.ToString() : "null");
+                                        throw new Exception($"Sheet: {SheetNameFrame} - Row: {iRow} => Invalid field type -> Expected: {sDataTypeExpected}  Actual: {sDataTypeActual}");
                                     }
                                 }
                                 ++iRow;
